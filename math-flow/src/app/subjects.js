0c7d4606f226c971946b6fb0e328c4b94c83f1be
REARU0924 <hissoribeatbox@gmail.com>
2025-08-17 19:00:58 +0900

    III　C 削除

--- math-flow/src/app/subjects.js
+++ math-flow/src/app/subjects.js
@@ -3,6 +3,4 @@ export  const subjects = [
     { label: 'A', value: '数A' },
     { label: 'll', value: '数II＊準備中' },
     { label: 'B', value: '数B＊準備中' },
-    { label: 'lll', value: '数III＊準備中' },
-    { label: 'C', value: '数C＊準備中' }
   ]
