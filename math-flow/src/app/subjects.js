2857422e7ba2132165627096f9ce8c261cedb3c3
岡田玲亜流 <okadarearu@okadareiaryuunoMacBook-Air.local>
2025-10-04 18:42:19 +0900

    suretu

--- math-flow/src/app/subjects.js
+++ math-flow/src/app/subjects.js
@@ -2,5 +2,5 @@ export  const subjects = [
     { label: 'l', value: '数I' },
     { label: 'A', value: '数A' },
     { label: 'II', value: '数II' },
-    { label: 'BC', value: '数B + C＊準備中' },
+    { label: 'BC', value: '数B + C' },
   ]
